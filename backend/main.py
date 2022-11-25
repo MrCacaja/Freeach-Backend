e3f8d6f0586df786c9e0eea8382df8cbb8501709
MrCacaja <36420999+MrCacaja@users.noreply.github.com>
2022-11-24 21:37:37 -0300

    CORS livre

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, Body
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 import crud
@@ -13,6 +14,15 @@ from exceptions import UsuarioException, TipoHabilidadeException, TipoServicoExc
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # usuário
 
